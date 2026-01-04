eae43f95df69f1037ee1b24dfe166a73575e5344
subishga06 <subishgavs@gmail.com>
2026-01-04 19:56:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import './../styles/App.css';
+import Greeting from "./Greeting";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Greeting />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
